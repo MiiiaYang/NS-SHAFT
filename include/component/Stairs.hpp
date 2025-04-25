2e59f0d86fc346439e75953b45382fe30db71922
miiia.yang <i0987801999@gmail.com>
2025-04-25 14:35:19 +0800

    feat: create crack stairs in phase third

--- include/component/Stairs.hpp
+++ include/component/Stairs.hpp
@@ -12,8 +12,9 @@
 
 class Stairs : public Util::GameObject {
 public:
-  enum class StairType { BASE = 0, SPIKE = 1 };
+  enum class StairType { BASE = 0, SPIKE = 1, CRACK = 2 };
   StairType m_Type;
+  int disappear_countdown = 0;
   explicit Stairs(const StairType type) {
     m_Type = type;
     switch (type) {
@@ -23,13 +24,16 @@ public:
     case StairType::SPIKE:
       m_ImagePath = (GA_RESOURCE_DIR "/stairs/spiked_stairs.png");
       break;
+    case StairType::CRACK:
+      disappear_countdown = 180;
+      m_ImagePath = (GA_RESOURCE_DIR "/stairs/disappear_stairs.png");
+      break;
     }
     m_Drawable = std::make_shared<Util::Image>(m_ImagePath);
   }
   void SetImage(const std::string &ImagePath) {
     m_ImagePath = ImagePath;
     m_Drawable = std::make_shared<Util::Image>(m_ImagePath);
-
   }
   void SetPosition(const glm::vec2 &Position) {
     m_Transform.translation = Position;
