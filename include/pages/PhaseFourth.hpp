402d1087949fd39d78d4dc3bd9797414a427714b
112590015 <t112590015@ntut.edu.tw>
2025-05-02 15:48:27 +0800

    feat: add drop trap

--- include/pages/PhaseFourth.hpp
+++ include/pages/PhaseFourth.hpp
@@ -3,12 +3,15 @@
 
 #include "Phase.hpp"
 #include "component/stairs.hpp"
+#include "component/FallingObstacle.hpp"
 class PhaseFourth: public Phase {
 public:
     int spikeCount = 0;
     std::shared_ptr<Stairs> m_lastDamagingStair = nullptr;
     int m_initialTimer = 0;
     explicit PhaseFourth() { phase = Enum::PhaseEnum::PhaseFourth; }
+
+    std::vector<std::shared_ptr<FallingObstacle>> m_obstacles;
 
     void Start() override;
     void Update() override;
